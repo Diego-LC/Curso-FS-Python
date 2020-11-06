65f8126be3adb16f82cd954480fefd00ab4fdea1
Diego Labrin Curin <di3golabrin@gmail.com>
2020-11-05 23:14:26 -0300

    update

--- Javascript/Fundamentos-basicos.js
+++ Javascript/Fundamentos-basicos.js
@@ -13,7 +13,17 @@ console.log(a);
 
 //Consigue pares hasta 1000: Escribe una función que entregue la suma de todos los 
 //números pares del 1 al 1000 - Puedes usar un operador de módulo para este ejercicio. 
+
 function SumPares(){
     sum=0
-    for(var i=2; i<=1000; i+2)
+    for(var i=0; i<=1000; i+=2){
+        sum=sum+i;
+    }
+    console.log(sum);
 }
+a=SumPares();
+
+
+
+/*Itera un array: Escribe una función que devuelva la suma de todos los valores 
+dentro de un array (ej:  [1,2,5] retorna 8. [-5,2,5,12] retorna 14).*/ 
